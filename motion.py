f7aee2300df4acc5c1eddb27135b288d6a46871b
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2023-01-07 17:17:38 +0300

    Create motion.py

---
+++ motion.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('video.mp4')
+
+# initializing subtractor
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+
+while(1):
+	ret, frame = cap.read()	
+
+	# applying on each frame
+	fgmask = fgbg.apply(frame)
+
+	cv2.imshow('mask', fgmask)
+	cv2.imshow('frame', frame)
+	k = cv2.waitKey(30) & 0xff
+	if k == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
